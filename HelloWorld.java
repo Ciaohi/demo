6ad3fb08b0c4cfa26f8bdc576277d7993db18928
ciaohi <465105975@qq.com>
2019-09-20 14:46:40 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+ public class Hello World{
+	public static void main(String[] args){
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+
+      }
+} 
